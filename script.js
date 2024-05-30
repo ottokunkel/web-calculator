d775c88430ce1a4cd2757a029335e74082ee1b86
ottokunkel <ottokunkel034@gmail.com>
2024-05-29 21:58:16 -0500

    initial boilerplate code

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction(){
+    console.log("hello world");
+}
+
+function addOperator(operator, array){
+    
+}
+
+function main(){
+    let calculatorOps = [];  
+     
+}
+
+main();
